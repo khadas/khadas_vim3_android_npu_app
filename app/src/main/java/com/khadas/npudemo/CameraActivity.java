a405bf54af366718640db13dc663a16907d22630
Xiong Zhang <xiong.zhang@wesion.com>
2023-06-30 16:56:18 +0800

    add model yolo_v7_tiny
    Signed-off-by: Xiong Zhang <xiong.zhang@wesion.com>

--- app/src/main/java/com/khadas/npudemo/CameraActivity.java
+++ app/src/main/java/com/khadas/npudemo/CameraActivity.java
@@ -112,7 +112,8 @@ public abstract class CameraActivity extends AppCompatActivity implements Camera
         DET_YOLOFACE_V2,
         DET_YOLO_V2,
         DET_YOLO_V3,
-        DET_YOLO_TINY
+        DET_YOLO_TINY,
+        DET_YOLO_V7_TINY
     }
     static ModeType mode_type;
 
@@ -168,6 +169,12 @@ public abstract class CameraActivity extends AppCompatActivity implements Camera
                     in = assmgr.open("yolotiny_88.nb");
                 } else {
                     in = assmgr.open("yolotiny_99.nb");
+                }
+            }  if(mode_type == ModeType.DET_YOLO_V7_TINY) {
+                if(mStrboard.equals("kvim3")) {
+                    in = assmgr.open("yolov7_tiny_88.nb");
+                } else {
+                    in = assmgr.open("yolov7_tiny_99.nb");
                 }
             }  if(mode_type == ModeType.DET_YOLOFACE_V2) {
                 if(mStrboard.equals("kvim3")) {
@@ -290,6 +297,14 @@ public abstract class CameraActivity extends AppCompatActivity implements Camera
                 copyNbFile(this, "yolotiny_88.nb");
             } else {
                 copyNbFile(this, "yolotiny_99.nb");
+            }
+            setmoderesult = inceptionv3.npu_det_set_model(mode_type.ordinal());
+        }
+        if(mode_type == ModeType.DET_YOLO_V7_TINY) {
+            if(mStrboard.equals("kvim3")) {
+                copyNbFile(this, "yolov7_tiny_88.nb");
+            } else {
+                copyNbFile(this, "yolov7_tiny_99.nb");
             }
             setmoderesult = inceptionv3.npu_det_set_model(mode_type.ordinal());
         }
--- app/src/main/java/com/khadas/npudemo/MainActivity.java
+++ app/src/main/java/com/khadas/npudemo/MainActivity.java
@@ -17,18 +17,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button button_yolov3;
     private Button button_yolov2;
     private Button button_yoloface;
+    private Button button_yolov7_tiny;
     public  static  final String Intent_key="modetype";
     private static final String TAG = "MainActivity";
     AlertDialog.Builder alertDialog0;
     AlertDialog.Builder alertDialog;
     AlertDialog.Builder alertDialog2;
     AlertDialog.Builder alertDialog3;
+    AlertDialog.Builder alertDialog4;
 
     public enum ModeType {
         DET_YOLOFACE_V2,
         DET_YOLO_V2,
         DET_YOLO_V3,
-        DET_YOLO_TINY
+        DET_YOLO_TINY,
+        DET_YOLO_V7_TINY
     }
 
 
@@ -45,11 +48,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         button_yolov3 = (Button) findViewById(R.id.button_yolov3);
         button_yolov2 = (Button) findViewById(R.id.button_yolov2);
         button_yoloface = (Button) findViewById(R.id.button_yoloface);
+        button_yolov7_tiny = (Button) findViewById(R.id.button_yolov7_tiny);
 
         button_yolotiny.setOnClickListener(this);
         button_yolov3.setOnClickListener(this);
         button_yolov2.setOnClickListener(this);
         button_yoloface.setOnClickListener(this);
+        button_yolov7_tiny.setOnClickListener(this);
 
         alertDialog0 = new AlertDialog.Builder(MainActivity.this);
         alertDialog0.setTitle("prompt");
@@ -127,7 +132,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 })
                 .create();
 
-
+        alertDialog0 = new AlertDialog.Builder(MainActivity.this);
+        alertDialog0.setTitle("prompt");
+        alertDialog0.setMessage("yolov7_tiny image recognition model will run");
+        alertDialog0.setNegativeButton("cancel", new DialogInterface.OnClickListener() {//添加取消
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Log.e(TAG, "AlertDialog cancel");
+                //onClickNo();
+            }
+        })
+                .setPositiveButton("ok", new DialogInterface.OnClickListener() {//添加"Yes"按钮
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Log.e(TAG, "AlertDialog ok");
+                        onClickYolov7Tiny();
+                    }
+                })
+                .create();
 
     }
 
@@ -161,6 +183,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 //onClickButton2(v);
                 alertDialog3.setCancelable(false);//点击空白处之后弹出框不会消失
                 alertDialog3.show();
+                buttonSetFocus(v);
+                break;
+            case R.id.button_yolov7_tiny:
+                Log.e(TAG, "button_yolov7_tiny");
+                //onClickButton1(v);
+                alertDialog0.setCancelable(false);//点击空白处之后弹出框不会消失
+                alertDialog0.show();
                 buttonSetFocus(v);
                 break;
             default:
@@ -206,6 +235,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Log.e(TAG, "button_yoloface enter ");
         Intent intent = new Intent(this,ClassifierActivity.class);
         intent.putExtra(Intent_key,ModeType.DET_YOLOFACE_V2.ordinal());
+        startActivity(intent);
+    }
+
+    private void onClickYolov7Tiny() {
+        //处理逻辑
+        Log.e(TAG, "button_yolov7_tiny enter ");
+        Intent intent = new Intent(this,ClassifierActivity.class);
+        intent.putExtra(Intent_key,ModeType.DET_YOLO_V7_TINY.ordinal());
         startActivity(intent);
     }
 }
